89792fc20a9483760b7190f4d3ec950d2ca71195
giaki3003 <giaki3003@gmail.com>
2018-08-31 16:11:43 +0200

    Added proper Block size (in Bytes)

--- parser.cpp
+++ parser.cpp
@@ -144,7 +144,7 @@ static double getMem() {
 #endif
 
 #if defined BEETLE
-    static const size_t gHeaderSize = 80;
+    static const size_t gHeaderSize = 256;
     static auto kCoinDirName = ".Beetle";
     static const uint32_t gExpectedMagic = 0x63fbbab9;
 #endif
